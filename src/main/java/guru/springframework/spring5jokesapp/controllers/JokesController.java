686535c25a5c7c86ed99b5f65dd54c7e160b8f10
Nadine Dithmer <ndevelop@mail.de>
2022-05-05 10:38:35 +0200

    Change to using interface instead of implementation

--- src/main/java/guru/springframework/spring5jokesapp/controllers/JokesController.java
+++ src/main/java/guru/springframework/spring5jokesapp/controllers/JokesController.java
@@ -2,21 +2,22 @@ package guru.springframework.spring5jokesapp.controllers;
 
 
 import guru.springframework.spring5jokesapp.services.ChuckNorrisJokeService;
+import guru.springframework.spring5jokesapp.services.JokeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokesController {
-    private final ChuckNorrisJokeService chuckNorrisJokeService;
+    private final JokeService jokeService;
 
-    public JokesController(ChuckNorrisJokeService chuckNorrisJokeService) {
-        this.chuckNorrisJokeService = chuckNorrisJokeService;
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
     }
 
     @RequestMapping("/")
     public String getJoke(Model model) {
-        model.addAttribute("joke", chuckNorrisJokeService.getRandomJoke());
+        model.addAttribute("joke", jokeService.getRandomJoke());
         return "index";
     }
 }
