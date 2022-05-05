145093928a8c3c9ca5f4426478d723ab17d724a0
Nadine Dithmer <ndevelop@mail.de>
2022-05-05 10:23:27 +0200

    Add Joke Service

---
+++ src/main/java/guru/springframework/spring5jokesapp/services/ChuckNorrisJokeService.java
@@ -0,0 +1,20 @@
+package guru.springframework.spring5jokesapp.services;
+
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChuckNorrisJokeService implements JokeService {
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public ChuckNorrisJokeService() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String getRandomJoke(){
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
